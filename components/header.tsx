93acbf12838a806e8a219ce2081819e1775d5d87
service-tuansyho <service.tuansyho@gmail.com>
2025-09-26 17:06:30 +0700

    auto sync

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,8 @@
 import * as motion from "motion/react-client"
 import Link from "next/link";
 import { NavigationMenuDemo } from "./test"
+import Image from "next/image";
+import { MenuHeader } from "./header/header.menu";
 const box = {
     width: 100,
     height: 100,
@@ -10,19 +12,43 @@ const box = {
     backgroundPosition: "center",
     borderRadius: 5,
 }
+const ArrayImg = [
+    "https://d2jfx0w9sp915a.cloudfront.net/c1f0aaf9514e43e6735f3400437fa5cc",
+    "https://d2jfx0w9sp915a.cloudfront.net/4e3ba33aaf9ac735ef1827ebeaa396d0",
+    "https://d2jfx0w9sp915a.cloudfront.net/f187e82833a48e7b8755a8f437e9b5a6",
+    "https://d2jfx0w9sp915a.cloudfront.net/4e1df3cec158715438f9e694fc639f63",
+    "https://d2jfx0w9sp915a.cloudfront.net/bb52dbbc26d0d1ba0c2520823a12b0c6",
+    "https://d2jfx0w9sp915a.cloudfront.net/fdc89538b99b57e5aa87096df447d3ce"
+]
 
 const Header = () => {
     return (
-        <header className="w-full bg-gray-800 text-white p-4">
-            <motion.div
-                style={box}
-                animate={{ rotate: 360 }}
-                transition={{ duration: 1 }}
-            />
-            <h1 className="text-2xl font-bold"></h1>
-            <Link href="/" className="mr-4 hover:underline">Home</Link>
-            <Link href="/blog" className="hover:underline">Blog</Link>
-            <NavigationMenuDemo />
+        <header className="w-full relative">
+            {/* grid chia 6 ảnh */}
+            <div className="grid grid-cols-3 md:grid-cols-6">
+                {ArrayImg.map((img, index) => (
+                    <div
+                        key={index}
+                        className="relative w-full h-96 md:h-[500px]"
+                    >
+                        <Image
+                            src={img}
+                            alt={`Image ${index + 1}`}
+                            fill
+                            className="object-cover border border-white"
+                        />
+                    </div>
+                ))}
+            </div>
+            <div className="absolute top-[10px] flex items-center justify-center w-full gap-4">
+                <motion.div
+                    style={box}
+                    animate={{ rotate: 360 }}
+                    transition={{ duration: 1 }}
+                />
+                <MenuHeader />
+            </div>
+
         </header>
     );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd2jfx0w9sp915a.cloudfront.net',
+        port: '', // để trống nếu không có port
+        pathname: '/**', // cho phép tất cả path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
